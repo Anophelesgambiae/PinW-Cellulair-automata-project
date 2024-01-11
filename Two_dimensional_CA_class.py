427b30b4877667e933d69a5f67b74273e2f0ccea
Robert Monedula <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-11 16:59:23 +0100

    Add files via upload

--- Two_dimensional_CA_class.py
+++ Two_dimensional_CA_class.py
@@ -1,6 +1,9 @@
 
 import numpy as np
 from Square_CA_class import square_CA
+import msvcrt as ms
+import time
+import copy
 
 class two_dimension_CA(square_CA):
 
@@ -11,19 +14,40 @@ class two_dimension_CA(square_CA):
 
         # Initialise the field with only state 0.
         self.field = np.zeros((length, width), dtype = int)
-        self.timestep = 0
+
         # Populate the field with some other states.
         for row in range(0, len(self.field)):
             for column in range(0 ,len(self.field[row])):
                 # The p is a list with the frequency of each state in order of 0 and higher. 
-                state = np.random.choice(np.arange(0,2), p=[0.8, 0.2])
+                state = np.random.choice(np.arange(0,2), p=[0.6, 0.4])
                 self.field[row][column] = state
-
+        
+        print(self.field)
+        self.next_generation()
+    
+    # Return a string of the field.
     def __str__(self):
         return str(self.field)    
+    
+    def next_generation(self):
 
-    def Next_generation():
+        while True:
+            time.sleep(1)
+            old_field = copy.deepcopy(self.field)
+             
+            for row in range(0, len(old_field)):
+                for column in range(0 , len(old_field[row])):
+                    # Test
+                    if old_field[row][column-1] == 0:
+                        self.field[row][column] = 0
+                    else: self.field[row][column] = 1     
+
+            print(self.field)
+
+            if ms.kbhit():
+                if ord(ms.getch()) == 32:
+                    break 
 
 
-a = two_dimension_CA(2,8,20)
+a = two_dimension_CA(3,12)
 print(a)
