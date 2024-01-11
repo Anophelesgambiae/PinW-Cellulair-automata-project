78a7920de07a51d2b27d507c20144df06ba6e4e9
Robert Monedula <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-11 13:25:43 +0100

    Add files via upload

---
+++ CA_class.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+
+class CA():
+
+    def __init__(self) -> None:
+        print("Kaas")
+
+# Example
+class one_dimension_CA(CA):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+# Example
+class two_dimension_CA(CA):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+a = one_dimension_CA()
+
