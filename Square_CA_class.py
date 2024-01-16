ee518764acb6afde4f4e1195d1d32ebdf09857f0
H5N1 jackdaw <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-16 12:41:12 +0100

    Add files via upload

--- Square_CA_class.py
+++ Square_CA_class.py
@@ -4,5 +4,6 @@ from CA_class import CA
 
 class square_CA(CA):
 
-    def __init__(self, length):
+    def __init__(self, length, boundary_condition):
         self.length = length
+        self.boundary_condition = boundary_condition
