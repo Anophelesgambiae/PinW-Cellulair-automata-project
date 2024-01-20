4730dd2f469c3ee49629475174ba400528247a80
ploepiekaas <156079975+ploepiekaas@users.noreply.github.com>
2024-01-20 16:59:25 +0100

    One_dimensional_CA_class.py

--- One_dimensional_CA_class.py
+++ One_dimensional_CA_class.py
@@ -2,49 +2,91 @@ import numpy as np
 import time
 import copy
 import msvcrt as ms
+
 from Square_CA_class import square_CA
 
-# Create a class for a one dimensional field
+
 class one_dimension_CA(square_CA):
+    '''
+    Here a class for one dimensional CA's is created.
 
-    # Create a field initializer
-    def __init__(self,length,boundary_condition) -> None:
+    The inputs are as follows:
+    -length: An integer of the length of the field
+    -boundary-condition: "constant" for a system with borders
+     and "periodic" for a borderless field
+    -rule_number: The rule number used for the CA
+    '''
 
-        # Initialise the field as an array of zeros
+    def __init__(self, length, boundary_condition, rule_number) -> None:
+        '''Return the starting field and the dictionary of the CA''' 
+
+        # Create the dictionary for the rule. The input represents the
+        # element's neighbour, the element itself and the element's right
+        # neighbour
+        rule_dict = {"111":0, "110":0, "101":0, "100":0, 
+                     "011":0, "010":0, "001":0, "000":0}
+        binary_number = "{0:08b}".format(rule_number)
+        binary_list = list(binary_number)
+        key_items_list = ["111", "110", "101", "100",
+                          "011", "010", "001", "000"]
+        i = 0
+        for key in key_items_list:
+            rule_dict[key] = int(binary_list[i])
+            i += 1
+        self.rule_dict = rule_dict
+
+        # Initialize the field where the elements have a 50% chance to be a
+        # zero and 50% chance to be a one
         self.field = np.zeros(length, dtype = int)
+        for item in range(0, len(self.field)):
+            self.field[item] = np.random.choice(np.arange(0, 2), p=[0.5, 0.5])
 
-        # Set the value to 1 for some of the elements on the field
-        self.field[(length//2)] = 1
-        # for column in range(0, len(self.field)):
-            # self.field[column] = np.random.choice(np.arange(0, 2), p = [0.5, 0.5])
         print(self.field)
-        self.Next_generation(length,boundary_condition)
+        self.Next_generation(length, boundary_condition, rule_dict)
 
-    # Define how the next generation is created
-    def Next_generation(self,length,):
+    def Next_generation(self, length, boundary_condition, rule_dict):
+        ''' Return the next generation of the CA '''
+
         while True:
-            time.sleep(1)
-            rule_30 = {"111":0, "110":0, "101":0, "100":1, "011":1, "010":1, "001":1, "000":0}
+            time.sleep(1)            
             old_field = copy.deepcopy(self.field)
             match boundary_condition:
+
+                # Create a periodic border by adding %length when looking at
+                # the neighbours so that the first element's left neighbour
+                # will be the last element and the last element's right
+                # neighbour will be the first element
                 case "periodic":
-                    for column in range(0, len(self.field)):
-                        newstate = rule_30[str(old_field[(column-1) % len(old_field)])
-                                           + str(old_field[column])
-                                           + str(old_field[(column+1) % len(old_field)])]
-                        self.field[column] = newstate
+                    for element in range(0, length):
+                        newstate = rule_dict[
+                          str(old_field[(element-1) % length])
+                        + str(old_field[element])
+                        + str(old_field[(element+1) % length])
+                        ]
+                        self.field[element] = newstate
+
+                # Create a hard border by not changing the first and last
+                # element
                 case "constant":
-                    for column in range(1, len(self.field)-1):
-                        newstate = rule_30[str(old_field[(column-1)])
-                                           + str(old_field[column])
-                                           + str(old_field[(column+1)])]
-                        self.field[column] = newstate
+                    for element in range(1, length-1): 
+                        newstate = rule_dict[
+                          str(old_field[(element-1)])
+                        + str(old_field[element])
+                        + str(old_field[(element+1)])
+                        ]
+                        self.field[element] = newstate
+
             print(self.field)
+
+            # Stop the run when the spacebar is pressed
             if ms.kbhit():
                 if ord(ms.getch()) == 32:
                     break
 
-    # Return a string of the field
     def __str__(self):
+        '''Return a string of the field'''
+
         return str(self.field)
-a = one_dimension_CA(31,"constant")        
+
+Printed_CA = one_dimension_CA(int(input()), input(), int(input()))
+'''Return the CA for the given input'''
