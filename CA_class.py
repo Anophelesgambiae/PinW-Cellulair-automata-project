305fe8328fb106d0e7badf99a19e3c135aa0cad4
H5N1 jackdaw <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-24 19:48:09 +0100

    Add files via upload

--- CA_class.py
+++ CA_class.py
@@ -1,11 +1,14 @@
+
+import numpy as np
+
 class CA():
-    '''Create an abstract class for the CA'''
 
     def __init__(self):
         None
 
-    def Next_generation(self):
-        return
 
-    def Neighbours(self, cell):
-        return
+
+
+
+
+
