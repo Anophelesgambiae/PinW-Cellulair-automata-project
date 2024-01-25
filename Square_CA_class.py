dd32787719f06957f2f735ff356cbb5c3742161c
H5N1 jackdaw <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-25 13:51:14 +0100

    Add files via upload

--- Square_CA_class.py
+++ Square_CA_class.py
@@ -5,6 +5,7 @@ from CA_class import CA
 '''
 The square class are the CA classes with square as cells. 
 There exists the one-dimensional and two-dimensional square CA (and of course more).
+
 All square CA have a length and a boundary condition. 
 These are the different boundary conditions:
 - periodic
@@ -13,6 +14,6 @@ These are the different boundary conditions:
 '''
 class square_CA(CA):
 
-    def __init__(self, length, boundary_condition):
+    def __init__(self, length: int, boundary_condition: str):
         self.length = length
         self.boundary_condition = boundary_condition
