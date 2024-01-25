db78b3d4b91ad91c019b5558c1c57a3d6e9a6cfe
H5N1 jackdaw <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-25 16:05:49 +0100

    Add files via upload

--- Two_dimensional_CA_class.py
+++ Two_dimensional_CA_class.py
@@ -3,8 +3,11 @@ import numpy as np
 import msvcrt as ms
 import time
 import copy
+import types
+import matplotlib.pyplot as plot
 
 from Square_CA_class import square_CA
+
 
 '''
 This a class of a two dimensional Cellulair automata (CA). The field is thus a retangle with a predefined length and width.
@@ -29,42 +32,45 @@ class two_dimension_CA(square_CA):
     '''
     Make a field with a random state on each cell. Calls the next_generation method.
     '''
-    def __init__(self, length, width, rule, neighbourhood_rule, boundary_condition):
+    def __init__(self, length: int, width: int, rule: str, neighbourhood_rule: str, 
+                 boundary_condition: str, timesteps: int):
 
-        if length < 0 or width < 0:
+        if length <= 0 or width <= 0:
             raise ValueError("length and width must be greather than zero.")
-
-        self.width = width
-        self.neighbourhood_rule = neighbourhood_rule
-        self.rule = rule
+        
+        self.width: int = width
+        self.neighbourhood_rule: str = neighbourhood_rule
+        self.rule: str = rule
 
         # Initialise the field as an array of zeros.
-        self.field = np.zeros((length, width), dtype = int)
-
+        self.field: np.array = np.zeros((length, width), dtype = int)
         # Set the value to 1 for some of the elements on the field
         for row in range(0, length):
             for column in range(0 , width):
                 # The p is a list with the frequency of each state in order of 0 and higher. 
-                state = np.random.choice(np.arange(0,2), p=[0.5, 0.5])
+                state: int = np.random.choice(np.arange(0,2), p=[0.5, 0.5])
                 self.field[row][column] = state
-        # Remove later
-        print(self.field)
 
-        self.next_generation(length, width, rule, neighbourhood_rule, boundary_condition)
+        self.next_generation(length, width, rule, neighbourhood_rule, boundary_condition, timesteps)
     
     # Return a string of the field.
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.field)    
     '''
     Define for all cells on the field the new state (0 or 1). This function run until the user press space bar.
     You can define new boundary_condition to make a new case with a new name in a string. In this case you define with a for loop
     which cells can change.
     '''
-    def next_generation(self, length, width, rule, neighbourhood_rule, boundary_condition):
-
-        while True:
-            time.sleep(1)
-            old_field = copy.deepcopy(self.field)
+    def next_generation(self, length: int, width: int, rule: str, neighbourhood_rule: str, boundary_condition: str, timesteps: int):
+        
+        for t in range(0, timesteps):
+            
+            # Stops the run when you press space or esc.
+            if ms.kbhit():
+                if ord(ms.getch()) in [27,32]:
+                    quit()
+            
+            old_field: np.array = copy.deepcopy(self.field) 
             
             match boundary_condition:
                 case "periodic":
@@ -80,15 +86,11 @@ class two_dimension_CA(square_CA):
                             self.set_new_state(length, width, old_field, row, column, rule, neighbourhood_rule)
 
                 case _:
-                    raise NameError(str(boundary_condition) + " " + "is not defined in the next_generation method")
+                    raise NameError(str(boundary_condition) + " " + "is not defined in the next_generation method, perhaps you made a typo")
 
-            # Remove later
-            print(self.field)
-
-            if ms.kbhit():
-                if ord(ms.getch()) == 32:
-                    break
-                    
+        print("CA has done " + str(timesteps) + " timesteps")    
+        super().display_CA(self.field)
+                            
     '''
     Set the new state (0 or 1) of the cel on the given row and column. 
     With the game of life the following rules exists:
@@ -100,7 +102,7 @@ class two_dimension_CA(square_CA):
     You can define new rules to make a new case with a new name in a string. With if statements you can tell when living cells die
     and when dead cells born. 
     '''
-    def set_new_state(self, length, width, old_field, row, column, rule, neighbourhood_rule):
+    def set_new_state(self, length: int, width: int, old_field: np.array, row: int, column: int, rule: str, neighbourhood_rule: str):
 
         old_state = old_field[row][column]
 
@@ -119,7 +121,7 @@ class two_dimension_CA(square_CA):
                 else: new_state = old_state 
 
             case _:
-                raise NameError(str(rule) + " " + "is not defined in the set_new_state method")    
+                raise NameError(str(rule) + " " + "is not defined in the set_new_state method, perhaps you made a typo")    
 
         self.field[row][column] = new_state    
 
@@ -129,7 +131,7 @@ class two_dimension_CA(square_CA):
     You can define new rules to make a new case with a new name in a string. You can define which cells are calculated.
     Example: old_field[3][5] returns the state of a cell on row 3 and column 5. 
     '''
-    def return_neighbour_sum(self, length, width, old_field, row, column, neighbourhood_rule):
+    def return_neighbour_sum(self, length: int, width: int, old_field: np.array, row: int, column: int, neighbourhood_rule: str) -> int:
         match neighbourhood_rule:
             case "Moore":
                     neighbour_sum = old_field[(row-1) % length][(column-1) % width] + \
@@ -146,17 +148,20 @@ class two_dimension_CA(square_CA):
                                     old_field[row][(column+1) % width] + \
                                     old_field[(row+1) % length][column] 
             case _:
-                raise NameError(str(neighbourhood_rule) + " " + "is not defined in the return_neighbour_sum method")
+                raise NameError(str(neighbourhood_rule) + " " + "is not defined in the return_neighbour_sum method, perhaps you made a typo" )
 
         return neighbour_sum
 
 # Make a two dimensional CA with the inputs of the user.
-dimensions = input("Give the length and width of the CA with a space: ").split(" ")
+dimensions = input("What is the length and width of the CA? ").split(" ")
 length = int(dimensions[0])
 width = int(dimensions[1])
-rules = input("Give the rule, neighbourhood rule and boundary condition with a space: ").split(" ")
+rules = input("What is the rule, neighbourhood rule and boundary condition?: ").split(" ")
 rule = rules[0]
 neighbourhood_rule = rules[1]
 boundary_condition = rules[2] 
+timesteps = int(input("How many timesteps you want that the CA runs: ") )
 
-CA = two_dimension_CA(length, width, rule, neighbourhood_rule, boundary_condition)
+CA = two_dimension_CA(length, width, rule, neighbourhood_rule, boundary_condition, timesteps)
+
+
