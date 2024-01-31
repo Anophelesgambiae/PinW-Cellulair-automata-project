3b0d0b5cf788682c2882d7934bf51645c141c8a7
H5N1 jackdaw <102152257+Anophelesgambiae@users.noreply.github.com>
2024-01-31 12:35:27 +0100

    Add files via upload

--- Square_CA_class.py
+++ Square_CA_class.py
@@ -19,7 +19,7 @@ The classes under the square classes use the display_CA method.
 class square_CA(CA):
 
     def __init__(self, length: int, boundary_condition: str):
-        self.length: = length
+        self.length = length
         self.boundary_condition = boundary_condition
 
     def display_CA(self, field):
